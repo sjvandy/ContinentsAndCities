814b7a352459be333673f604d2b8462a3eb9b2e7
Steven <Vandegrift>
2025-02-14 13:44:47 -0600

    Complete Codecademy Course

--- src/Continents.java
+++ src/Continents.java
@@ -1,6 +1,31 @@
 public class Continents {
 // This program prints out a continent and its largest city based on input
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int continent = 4;
+        switch (continent) {
+            case 1:
+                System.out.println("North America: Mexico City, Mexico");
+                break;
+            case 2:
+                System.out.println("South America: Sao Paulo, Brazil");
+                break;
+            case 3:
+                System.out.println("Europe: Moscow, Russia");
+                break;
+            case 4:
+                System.out.println("Africa: Lagos, Nigeria");
+                break;    
+            case 5:
+                System.out.println("Asia: Shanghai, China");
+                break;
+            case 6:
+                System.out.println("Australia: Sydney, Australia");
+                break;
+            case 7:
+                System.out.println("Antarctica: McMurdo Station, US");
+            default:
+                System.out.println("Undefined continent!");
+                break;
+        }
     }
 }
