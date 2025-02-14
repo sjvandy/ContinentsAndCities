13c2d6f177793101a8609908a145f9bd5519d72c
Steven <Vandegrift>
2025-02-14 14:00:27 -0600

    Incorperate Scanner and user input to select continent

--- src/Continents.java
+++ src/Continents.java
@@ -1,31 +1,47 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Continents {
 // This program prints out a continent and its largest city based on input
     public static void main(String[] args) {
-        int continent = 4;
-        switch (continent) {
-            case 1:
+        Scanner scanner = new Scanner(System.in);
+        String[] options = {"north america", "south america", "europe", "africa", "asia", "australia", "antarctica"};
+        String continent;
+        
+        while (true) {
+            System.out.print("Enter a continent to see the largest city in it!: ");
+            continent = scanner.nextLine();
+            if (Arrays.asList(options).contains(continent)) {
+                break;
+            } else {
+                System.out.println("Invalid continent. Please try again.");
+            }
+        }
+        switch (continent.toLowerCase()) {
+            case "north america":
                 System.out.println("North America: Mexico City, Mexico");
                 break;
-            case 2:
+            case "south america":
                 System.out.println("South America: Sao Paulo, Brazil");
                 break;
-            case 3:
+            case "europe":
                 System.out.println("Europe: Moscow, Russia");
                 break;
-            case 4:
+            case "africa":
                 System.out.println("Africa: Lagos, Nigeria");
                 break;    
-            case 5:
+            case "asia":
                 System.out.println("Asia: Shanghai, China");
                 break;
-            case 6:
+            case "australia":
                 System.out.println("Australia: Sydney, Australia");
                 break;
-            case 7:
+            case "antarctica":
                 System.out.println("Antarctica: McMurdo Station, US");
             default:
                 System.out.println("Undefined continent!");
                 break;
         }
+        scanner.close();
     }
 }
